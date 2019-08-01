e84099adcd3f6831b19c41e2b90fb95c3dd537a4
Sam Crawford <sam.crawford82@gmail.com>
2019-08-01 13:24:54 -0400

    Fixed InstanceModel

--- code/drasil-theory/Theory/Drasil/InstanceModel.hs
+++ code/drasil-theory/Theory/Drasil/InstanceModel.hs
@@ -1,15 +1,15 @@
-{-# LANGUAGE TemplateHaskell #-}
-module Theory.Drasil.InstanceModel
-  ( InstanceModel
-  , im, imNoDeriv, imNoRefs, imNoDerivNoRefs
-  , inCons, outCons, imOutput, imInputs -- FIXME, these should be done via lenses
-  , Constraints
+{-# LANGUAGE Rank2Types, ScopedTypeVariables, TemplateHaskell #-}
+module Theory.Drasil.InstanceModel (Constraints, InstanceModel, getEqMod,
+  im, imNoDeriv, imNoRefs, imNoDerivNoRefs,
+  inCons, outCons, imOutput, imInputs -- FIXME, these should be done via lenses
   ) where
 
 import Language.Drasil
 import Data.Drasil.IdeaDicts (inModel)
+import Theory.Drasil.ModelKinds (ModelKinds(..), elim_mk, set_mk)
 
-import Control.Lens ((^.), makeLenses, view)
+import Control.Lens (Lens', (^.), lens, makeLenses, set, to, view)
+import Data.Maybe (mapMaybe) 
 
 type Inputs = [QuantityDict]
 type Output = QuantityDict
@@ -22,7 +22,7 @@ type InputConstraints  = Constraints
 
 -- | An Instance Model is a RelationConcept that may have specific input/output
 -- constraints. It also has attributes like derivation, source, etc.
-data InstanceModel = IM { _rc :: RelationConcept
+data InstanceModel = IM { _mk :: ModelKinds
                         , _imInputs :: Inputs
                         , _inCons :: InputConstraints
                         , _imOutput :: Output
@@ -35,12 +35,19 @@ data InstanceModel = IM { _rc :: RelationConcept
                         }
 makeLenses ''InstanceModel
 
-instance HasUID             InstanceModel where uid = rc . uid
-instance NamedIdea          InstanceModel where term = rc . term
-instance Idea               InstanceModel where getA = getA . view rc
-instance Definition         InstanceModel where defn = rc . defn
-instance ConceptDomain      InstanceModel where cdom = cdom . view rc
-instance ExprRelat          InstanceModel where relat = relat . view rc
+lens_mk :: forall a. Lens' QDefinition a -> Lens' RelationConcept a -> Lens' InstanceModel a
+lens_mk lq lr = lens g s
+    where g :: InstanceModel -> a
+          g i = elim_mk lq lr (i ^. mk)
+          s :: InstanceModel -> a -> InstanceModel
+          s i x = set mk (set_mk (i ^. mk) lq lr x) i
+
+instance HasUID             InstanceModel where uid = lens_mk uid uid
+instance NamedIdea          InstanceModel where term = lens_mk term term
+instance Idea               InstanceModel where getA = elim_mk (to getA) (to getA) . view mk
+instance Definition         InstanceModel where defn = lens_mk defn defn
+instance ConceptDomain      InstanceModel where cdom = elim_mk (to cdom) (to cdom) . view mk
+instance ExprRelat          InstanceModel where relat = elim_mk (to relat) (to relat) . view mk
 instance HasDerivation      InstanceModel where derivations = deri
 instance HasReference       InstanceModel where getReferences = ref
 instance HasShortName       InstanceModel where shortname = lb
@@ -60,24 +67,30 @@ im :: RelationConcept -> Inputs -> InputConstraints -> Output ->
   OutputConstraints -> [Reference] -> Maybe Derivation -> String -> [Sentence] -> InstanceModel
 im rcon _ _  _ _  [] _  _  = error $ "Source field of " ++ rcon ^. uid ++ " is empty"
 im rcon i ic o oc r der sn = 
-  IM rcon i ic o oc r der (shortname' sn) (prependAbrv inModel sn)
+  IM (OthModel rcon) i ic o oc r der (shortname' sn) (prependAbrv inModel sn)
 
 -- | Smart constructor for instance models; no derivation
 imNoDeriv :: RelationConcept -> Inputs -> InputConstraints -> Output -> 
   OutputConstraints -> [Reference] -> String -> [Sentence] -> InstanceModel
 imNoDeriv rcon _ _  _ _ [] _  = error $ "Source field of " ++ rcon ^. uid ++ " is empty"
 imNoDeriv rcon i ic o oc r sn =
-  IM rcon i ic o oc r Nothing (shortname' sn) (prependAbrv inModel sn)
+  IM (OthModel rcon) i ic o oc r Nothing (shortname' sn) (prependAbrv inModel sn)
 
 -- | Smart constructor for instance models; no references
 imNoRefs :: RelationConcept -> Inputs -> InputConstraints -> Output -> 
   OutputConstraints -> Maybe Derivation -> String -> [Sentence] -> InstanceModel
 imNoRefs rcon i ic o oc der sn = 
-  IM rcon i ic o oc [] der (shortname' sn) (prependAbrv inModel sn)
+  IM (OthModel rcon) i ic o oc [] der (shortname' sn) (prependAbrv inModel sn)
 
 -- | Smart constructor for instance models; no derivations or references
 imNoDerivNoRefs :: RelationConcept -> Inputs -> InputConstraints -> Output -> 
   OutputConstraints -> String -> [Sentence] -> InstanceModel
 imNoDerivNoRefs rcon i ic o oc sn = 
-  IM rcon i ic o oc [] Nothing (shortname' sn) (prependAbrv inModel sn)
+  IM (OthModel rcon) i ic o oc [] Nothing (shortname' sn) (prependAbrv inModel sn)
 
+-- | Get equational models from a list of instance models
+getEqMod :: [InstanceModel] -> [QDefinition]
+getEqMod = mapMaybe isEqMod . map (view mk)
+  where
+    isEqMod (EquationalModel f) = Just f
+    isEqMod _                   = Nothing
